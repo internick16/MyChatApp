494e43a77a6bab32f1c6f0c5bc9b96191fb2eb8c
Jose Trinidad Garcia <internick12@gmail.com>
2019-11-11 23:23:46 -0600

    Initial Commit

---
+++ MyChatApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MyChatApp
+//
+//  Created by Trinidad Garcia on 11/11/19.
+//  Copyright © 2019 Trinidad Garcia. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
